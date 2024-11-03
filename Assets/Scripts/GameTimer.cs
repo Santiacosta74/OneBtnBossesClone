7167dc9671112955438992719cbdf21601968969
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2024-11-03 20:22:32 -0300

    Timer_Script_Canvas

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro; 
+
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText; 
+    private float elapsedTime = 0f; 
+    private bool isRunning = true; 
+
+    void Update()
+    {
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(elapsedTime / 60); 
+        int seconds = Mathf.FloorToInt(elapsedTime % 60); 
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); 
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+}
